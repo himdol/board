18b59a717b956cde6f0e42d9a6d73b5394096ed6
himdol <himdol8833@gmail.com>
2023-08-30 01:20:46 +0900

    start board

---
+++ src/test/java/com/sample/himchan/HimchanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.himchan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HimchanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
